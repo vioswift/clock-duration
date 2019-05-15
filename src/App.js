047d975ff860a433510c7e0988242e4d7f13cde9
Vioswift <kylewww@gmail.com>
2019-05-16 07:14:45 +1000

    Finished functions of the project
    left to do:
    + add Google ads
    + Link to website
    + Design tweaks

--- src/App.js
+++ src/App.js
@@ -5,13 +5,32 @@ function getTwoDigitDateFormat(monthOrDate) {
   return (monthOrDate < 10) ? '0' + monthOrDate : '' + monthOrDate;
 }
 
+function getCurrentDate() {
+  var currentYear = new Date().getFullYear();
+  var currentMonth = getTwoDigitDateFormat(new Date().getMonth() + 1);
+  var getDate = getTwoDigitDateFormat(new Date().getDate());
+  var currentDateString = currentYear + "-" + currentMonth + "-" + getDate;
+
+  return currentDateString;
+}
+
+function getCurrentTime(){
+  var hours = new Date().getHours();
+  var minutes = new Date().getMinutes();
+  hours = hours < 12 ? '0' + hours : hours;
+  var currentTimeString = hours + ':' + minutes;
+
+  return currentTimeString;
+}
+
 class App extends Component {
   state = {
-    startDate: "",
-    startTime: "",
-    endDate: "",
-    endTime: "",
-    totalDuration: "0"
+    currentDate: "",
+    startDate: getCurrentDate(),
+    startTime: getCurrentTime(),
+    endDate: getCurrentDate(),
+    endTime: getCurrentTime(),
+    totalDuration: "0 Days, 0 Hours, 0 Minutes"
   };
 
   startDateChange = (event) => {
@@ -58,7 +77,6 @@ class App extends Component {
     var date2 = new Date(date2Str);
 
     var res = Math.abs(date1 - date2) / 1000;
-    console.log("res= " + res);
 
     // get total days between two dates
     var days = Math.floor(res / 86400);                      
@@ -69,7 +87,6 @@ class App extends Component {
     // get minutes
     var minutes = Math.floor(res / 60) % 60;
 
-    // Convert back to days and return
     this.setState({
       totalDuration: days + " Days, " + hours + " Hours, " + minutes + " Minutes"
     });
@@ -78,63 +95,89 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-          <h1 className="h1">ClockDuration</h1>
-          <hr/>
+          <NavBar/>
+          <h1>ClockDuration</h1>
 
           <div className="container">
             <div className="row">
               <div className="col-md-6 offset-md-3">
-                  <div className="col-md-7">
-                    <h2 className="h2 float-left">Start</h2>
+                <hr/>
+
+                {/* start date and time */}
+                <div className="col-md-7">
+                  <h2 className="h2 float-left">Start</h2>
+                </div>
+                <div className="form-group row">
+                  <div className="col-md-6">
+                    <label htmlFor="startDate" className="label label-default float-left">Date: </label>
+                    <input type="date" name="startDate" className="form-control" defaultValue={this.state.startDate} onChange={this.startDateChange} required/>
                   </div>
-                  <div className="form-group row">
+
+                  <div className="col-md-6">
+                    <label htmlFor="startTime" className="label label-default float-left">Time: </label>
+                    <input type="time" name="startTime" className="form-control" defaultValue={this.state.startTime} onChange={this.startTimeChange} required/>
+                  </div>
+                </div>
+
+                <hr/>
+
+                {/* end date and time */}
+                <div className="col-md-7">
+                  <h2 className="h2 float-left">End</h2>
+                </div>
+                <div className="form-group row">
                     <div className="col-md-6">
-                      <label htmlFor="startDate" className="label label-default float-left">Date: </label>
-                      <input type="date" name="startDate" className="form-control" onChange={this.startDateChange}/>
+                      <label htmlFor="endDate" className="label label-default float-left">Date: </label>
+                      <input type="date" name="endDate" className="form-control" defaultValue={this.state.endDate} onChange={this.endDateChange} required/>                      
                     </div>
 
                     <div className="col-md-6">
-                      <label htmlFor="startTime" className="label label-default float-left">Time: </label>
-                      <input type="time" name="startTime" className="form-control" onChange={this.startTimeChange}/>
+                      <label htmlFor="endTime" className="label label-default float-left">Time: </label>
+                      <input type="time" name="endTime" className="form-control" defaultValue={this.state.endTime} onChange={this.endTimeChange} required/>                                         
+                    </div>
+                </div>
+
+                {/* calculate button */}
+                <div className="form-group row">
+                  <div className="col-md-6 offset-md-3">
+                    <button type="button" className="btn btn-dark btn-lg btn-block" onClick={this.calculateTotalDuration}>Calculate</button>
+                  </div>
+                </div>
+
+                {/* total duration */}
+                <div className="form-group row">
+                  <div className="col-md-12">
+                    <div className="alert alert-info" role="alert">
+                        {this.state.totalDuration} 
                     </div>
                   </div>
-
-                  <hr/>
-
-                  <div className="col-md-7">
-                    <h2 className="h2 float-left">End</h2>
-                  </div>
-                  <div className="form-group row">
-                      <div className="col-md-6">
-                        <label htmlFor="endDate" className="label label-default float-left">Date: </label>
-                        <input type="date" name="endDate" className="form-control" onChange={this.endDateChange}/>                      
-                      </div>
-
-                      <div className="col-md-6">
-                        <label htmlFor="endTime" className="label label-default float-left">Time: </label>
-                        <input type="time" name="endTime" className="form-control" onChange={this.endTimeChange}/>                                         
-                      </div>
-                  </div>
-
-                  <div className="form-group row">
-                    <div class="col-md-6 offset-md-3">
-                      <button type="button" className="btn btn-dark" onClick={this.calculateTotalDuration}>Calculate</button>
-                    </div>
-                  </div>
-
-                  <div className="form-group row">
-                    <div class="col-md-12">
-                      <div class="alert alert-info" role="alert">
-                          {this.state.totalDuration}
-                      </div>
-                        
-                    </div>
-
-                  </div>
+                </div>
               </div>
             </div>
           </div>
       </div>
+    );
+  }
+}
+
+class NavBar extends React.Component {
+  render() {
+    return (
+      <div>
+        <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+          <a className="navbar-brand" href="http://google.com/">ClockDuration</a>
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNavDropdown">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <a className="nav-link" target="_blank" rel="noopener noreferrer" href="http://google.com/">Vioswift</a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </div> 
     );
   }
 }
